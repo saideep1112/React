a33d553ad28dda3f74077f1067aafe5cffb54dcb
saideep <gandesaideep.gs@gmail.com>
2024-06-01 10:41:12 +0530

    added git ignore file and ignited app

--- App.js
+++ App.js
@@ -1,15 +1,9 @@
-{
-  /* <div id="parent">
-  <div id="child">
-    <h1>I'm an h1 tag</h1>
-    <h2>I'm an h2 tag</h2>
-  </div>
-</div>; */
-}
+import React from "react";
+import ReactDOM from "react-dom";
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
+    React.createElement("h1", {}, "I'm an vamshi tag"),
     React.createElement("h2", {}, "I'm an h2 tag"),
   ]),
   React.createElement("div", { id: "child" }, [
