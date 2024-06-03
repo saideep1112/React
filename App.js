bda45f4000f1b5adffe44bbbe434bbf0e9ad82d4
saideep <gandesaideep.gs@gmail.com>
2024-06-03 12:41:14 +0530

    updated with latest changes.

--- App.js
+++ App.js
@@ -1,16 +1,35 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm an vamshi tag"),
-    React.createElement("h2", {}, "I'm an h2 tag"),
-  ]),
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag"),
-  ]),
-]);
+// const parent = React.createElement("div", { id: "parent" }, [
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", {}, "I'm an vamshi tag"),
+//     React.createElement("h2", {}, "I'm an h2 tag"),
+//   ]),
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", {}, "I'm an h1 tag"),
+//     React.createElement("h2", {}, "I'm an h2 tag"),
+//   ]),
+// ]);
+const JsxHeading = () => {
+  return (
+    <h1 id="heading" className="head">
+      Hello from JSX Heading
+    </h1>
+  );
+};
+
+const Heading = () => {
+  return (
+    <div className="header">
+      <h1 id="heading" className="head">
+        Hello from React component
+      </h1>
+      <JsxHeading />
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
-console.log(parent);
+root.render(<Heading />);
+console.log(Heading());
